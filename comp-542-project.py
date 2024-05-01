fa7606058a407d6abb7c06e91dc843e4ea8b2e28
ahollenbaugh <56135023+ahollenbaugh@users.noreply.github.com>
2024-04-30 20:53:14 -0700

    Can successfully read in dataset

---
+++ comp-542-project.py
@@ -0,0 +1,4 @@
+# Read in the dataset file
+import pandas
+dataset = pandas.read_csv("C:\\Users\\aghol\\OneDrive\\Desktop\\COMP 542\\AndroidAdware2017\\TotalFeatures-ISCXFlowMeter.csv", sep=',')
+print(dataset.head())
